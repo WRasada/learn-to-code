18b11b15c138fa8ec57d94fac5d007aa23a35790
Wesley <wesleyrasada@gmail.com>
2017-02-14 21:45:51 -0800

    Javascript Array exercise 3

--- JAVASCRIPT/Arrays/list.js
+++ JAVASCRIPT/Arrays/list.js
@@ -1,4 +1,4 @@
-var todos = ["Walk the dog"];
+var todos = [];
 
 var input = prompt("what would you like to do?")
 
@@ -7,12 +7,47 @@ var input = prompt("what would you like to do?")
 while (input !== "quit"){
 
   if (input === "list") {
-     console.log(todos);
+    listTodos();
   } else if (input === "new") {
-    var newTodo = prompt("Enter new todo.")
-    todos.push(newTodo);
+    addTodo();
+  } else if (input === "delete") {
+    deleteTodo();
   }
   input = prompt("what would you like to do?")
 }
 
 console.log("Ok, you quit the app")
+
+
+
+
+function listTodos(){
+  console.log("******************")
+    todos.forEach(function(todo, i){
+      console.log(i + ": " + todo);
+    });
+  console.log("******************")
+}
+
+function addTodo(){
+  var newTodo = prompt("Enter new todo.")
+  todos.push(newTodo);
+  console.log(newTodo + " added to list");
+}
+
+function deleteTodo(){
+  todos.forEach(function(todo, i, array){
+    console.log(i + ": " + todo);
+  });
+  var input = prompt("Which number would you like to delete?");
+  var number = parseInt(input);
+  console.log(number);
+  if (isNaN(number)) {
+    alert("I need a real number");
+  } else if (number > (todos.length - 1)){
+    alert("I'm sorry but that number isn't on the list");
+  } else {
+    todos.splice(number, 1);
+    console.log("Todo Removed");
+  }
+}
