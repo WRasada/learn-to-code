41120a082f3bae255e92ced57543e6d8b8e76c49
Wesley <wesleyrasada@gmail.com>
2017-02-20 19:15:01 -0800

    First Express App Exercise

--- Node_JS/FirstExpressApp/app.js
+++ Node_JS/FirstExpressApp/app.js
@@ -8,9 +8,23 @@ app.get('/', function(req, res){
   res.send("Hi there!");
 });
 // "/bye" => "Goodbye!"
-
+app.get('/bye', function(req, res){
+  res.send("goodbye!");
+});
 // "/dog" => "Meow!"
+app.get('/dog', function(req, res){
+  console.log("someone made a request to /dog");
+  res.send("Meow!");
+});
 
+app.get('/r/:subredditName', function(req, res){
+  var subreddit = req.params.subredditName;
+  res.send("WELCOME TO THE" + subreddit.toUpperCase() + " SUBREDDIT");
+});
+
+app.get('*', function(req, res){
+  res.send("You are a star!");
+});
 
 app.listen(3000, function(){
   console.log("Listening on port 3000");
