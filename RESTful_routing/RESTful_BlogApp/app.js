68bef0272e43c398053cc98614c0dd3cdd0c3687
Wesley <wesleyrasada@gmail.com>
2017-02-22 22:25:04 -0800

    Added EDIT and UPDATE Routes and view files

--- RESTful_routing/RESTful_BlogApp/app.js
+++ RESTful_routing/RESTful_BlogApp/app.js
@@ -1,7 +1,9 @@
 var express = require('express'),
+    methodOverride = require('method-override')
     bodyParser = require('body-parser'),
     mongoose = require('mongoose'),
     app = express();
+
 
 mongoose.connect('mongodb://localhost/restful_blog_app');
 
@@ -9,6 +11,7 @@ mongoose.connect('mongodb://localhost/restful_blog_app');
 app.set("view engine", 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 // MONGOOSE/MODEL CONFIG
 var blogSchema = new mongoose.Schema({
@@ -24,12 +27,64 @@ app.get('/', function(req, res){
   res.redirect('/blogs');
 });
 
+// INDEX ROUTE
 app.get('/blogs', function(req, res){
   Blog.find({}, function(err, blogs){
     if(err){
       console.log(err);
     } else {
       res.render('index', {blogs: blogs});
+    }
+  });
+});
+
+// NEW ROUTE
+app.get('/blogs/new', function(req, res){
+  res.render('new');
+});
+
+// CREATE ROUTE
+app.post('/blogs', function(req, res){
+  // create blog
+  Blog.create(req.body.blog, function(err, newBlog){
+    if(err){
+      res.render('new');
+    } else {
+      // then, redirect to index
+      res.redirect('/blogs');
+    }
+  });
+});
+
+// SHOW ROUTE
+app.get('/blogs/:id', function(req, res){
+  Blog.findById(req.params.id, function(err, foundBlog){
+    if(err){
+      res.redirect('/blogs');
+    } else {
+      res.render('show', {blog: foundBlog});
+    }
+  });
+});
+
+// EDIT ROUTE
+app.get('/blogs/:id/edit', function(req, res){
+  Blog.findById(req.params.id, function(err, foundBlog){
+    if(err){
+      res.redirect('/blogs');
+    } else {
+      res.render('edit', {blog: foundBlog});
+    }
+  });
+});
+
+// UPDATE ROUTE
+app.put('/blogs/:id', function(req, res){
+  Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+    if(err){
+      res.redirect('/blogs');
+    } else {
+      res.redirect('/blogs/' + req.params.id);
     }
   });
 });
