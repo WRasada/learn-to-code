cb132f8ce74a5a652e37b62e197d2735e93d4512
Wesley <wesleyrasada@gmail.com>
2017-02-23 19:30:58 -0800

    YelpCamp - Seeding the database

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -3,6 +3,9 @@ var express     = require('express'),
     bodyParser  = require('body-parser'),
     mongoose    = require('mongoose');
     Campground  = require('./models/campground');
+    seedDB      = require('./seeds');
+
+seedDB();
     
 mongoose.connect("mongodb://localhost/yelp_camp")
 app.use(bodyParser.urlencoded({extended: true}));
---
+++ YelpCamp/seeds.js
@@ -0,0 +1,59 @@
+var mongoose = require('mongoose');
+var Campground = require('./models/campground');
+var Comment = require('./models/comment');
+
+var data = [
+  {
+      name: "Cloud's Rest",
+      image: "http://www.americansouthwest.net/california/photographs700/clouds-yosemite2.jpg",
+      description: "Blah Blah Blah"
+  },
+  {
+      name: "Desert's Rest",
+      image: "http://www.americansouthwest.net/california/photographs700/clouds-yosemite2.jpg",
+      description: "Blah Blah Blah"
+  },
+  {
+      name: "Numbani's Rest",
+      image: "http://www.americansouthwest.net/california/photographs700/clouds-yosemite2.jpg",
+      description: "Blah Blah Blah"
+  }
+];
+
+function seedDB(){
+  //Remove all campgrounds
+  Campground.remove({}, function(err){
+    if(err){
+      console.log(err);
+    } else {
+      //Add a few campgrounds
+      data.forEach(function(seed){
+        Campground.create(seed, function(err, campground){
+          if(err){
+            console.log(err);
+          } else {
+            console.log("added a campground");
+            //create a comment
+            Comment.create(
+              {
+                  text: "This place is great! I wish there was internet",
+                  author: "Billy"
+              }, function(err, comment){
+              if(err){
+                console.log(err);
+              } else {
+                campground.comments.push(comment);
+                campground.save();
+                console.log("Created new comment");
+              }
+            });
+          }
+        });
+      });
+      console.log("Removed Campgrounds");
+    }
+  });
+
+}
+
+module.exports = seedDB;
