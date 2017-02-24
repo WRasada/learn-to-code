687304d69efea78360e9bc6c07c30d6cf0d75d1e
Wesley <wesleyrasada@gmail.com>
2017-02-23 22:29:58 -0800

    Added passport to app.js and user model

--- Authentication/AuthDemo/app.js
+++ Authentication/AuthDemo/app.js
@@ -5,10 +5,22 @@ var express               = require('express'),
     localStrategy         = require('passport-local'),
     passportLocalMongoose = require('passport-local-mongoose'),
     User                  = require('./models/user');
-    
+
 mongoose.connect('mongodb://localhost/auth_demo_app');
 
+
 var app = express();
+
+app.use(require('express-session')({
+  secret: "coding is the best and fun",
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serialize(User.serializeUser());
+passport.deserialize(User.deserializeUser());
 
   app.set('view engine', 'ejs');
 
