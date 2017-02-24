4615285d327adba623cbc92bb4a095e822f5a80f
Wesley <wesleyrasada@gmail.com>
2017-02-23 22:48:22 -0800

    Auth Demo - Added register page and user signup form

--- Authentication/AuthDemo/app.js
+++ Authentication/AuthDemo/app.js
@@ -11,6 +11,7 @@ mongoose.connect('mongodb://localhost/auth_demo_app');
 
 var app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(require('express-session')({
   secret: "coding is the best and fun",
   resave: false,
@@ -19,10 +20,13 @@ app.use(require('express-session')({
 app.use(passport.initialize());
 app.use(passport.session());
 
-passport.serialize(User.serializeUser());
-passport.deserialize(User.deserializeUser());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
-  app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');
+// ================
+//     Routes
+// ================
 
   app.get('/', function(req, res){
     res.render('home');
@@ -31,6 +35,31 @@ passport.deserialize(User.deserializeUser());
   app.get('/secret', function(req, res){
     res.render('secret');
   });
+
+// ================
+//  Auth Routes
+// ================
+
+// show signup form
+app.get('/register', function(req, res){
+  res.render("register");
+});
+
+app.post('/register', function(req, res){
+  req.body.username
+  req.body.password
+  User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+    if(err){
+      console.log(err);
+      return res.render('register');
+    } else {
+      passport.authenticate('local')(req, res, function(){
+        res.redirect('/secret');
+      });
+    }
+  });
+});
+
 
   app.listen(3000, function(){
     console.log("Server has started");
