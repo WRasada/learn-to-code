729b0d35c6b9dd950530290fe1a374fdfec09a73
Wesley <wesleyrasada@gmail.com>
2017-02-24 18:53:59 -0800

    YelpCamp - Added logout and fixed Nav links

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -74,7 +74,7 @@ app.get('/campgrounds/:id', function(req, res){
 // ==================
 //  COMMENTS ROUTES
 // ==================
-app.get('/campgrounds/:id/comments/new', function(req, res){
+app.get('/campgrounds/:id/comments/new', isLoggedIn, function(req, res){
   Campground.findById(req.params.id, function(err, campground){
     if(err){
       console.log(err);
@@ -84,7 +84,7 @@ app.get('/campgrounds/:id/comments/new', function(req, res){
   })
 });
 
-app.post('/campgrounds/:id/comments', function(req, res){
+app.post('/campgrounds/:id/comments', isLoggedIn, function(req, res){
     Campground.findById(req.params.id, function(err, campground){
       //lookup campground using ID
       if(err){
@@ -141,6 +141,19 @@ app.post('/login', passport.authenticate('local',
 }), function(req, res){
 
 });
+
+// Logout Route
+app.get('/logout', function(req, res){
+  req.logout()
+  res.redirect('/campgrounds');
+});
+
+function isLoggedIn(req, res, next){
+  if(req.isAuthenticated()){
+    return next()
+  }
+  res.redirect('/login');
+}
 
 app.listen(3000, function(){
   console.log("Yelp Camp has started on port 3000");
