4a222606f63aa9effdf4fac984e727bb891f4ffd
Wesley <wesleyrasada@gmail.com>
2017-02-25 12:09:17 -0800

    YelpCamp - Added helpful Flash messages

--- YelpCamp/middleware/index.js
+++ YelpCamp/middleware/index.js
@@ -4,17 +4,20 @@ middlewareObj.checkCampgroundOwnership = function(req, res, next) {
  if(req.isAuthenticated()){
         Campground.findById(req.params.id, function(err, foundCampground){
            if(err){
+               req.flash('error', 'Campground not found');
                res.redirect("back");
            }  else {
                // does user own the campground?
             if(foundCampground.author.id.equals(req.user._id)) {
                 next();
             } else {
+                req.flash('error', 'You do not have permission to do that');
                 res.redirect("back");
             }
            }
         });
     } else {
+        req.flash('error', 'You need to be logged in to do that.');
         res.redirect("back");
     }
 }
@@ -29,11 +32,13 @@ middlewareObj.checkCommentOwnership = function(req, res, next) {
             if(foundComment.author.id.equals(req.user._id)) {
                 next();
             } else {
+                req.flash('error', 'You do not have permission to do that');
                 res.redirect("back");
             }
            }
         });
     } else {
+        req.flash('error', 'You need to be logged in to do that');
         res.redirect("back");
     }
 }
@@ -42,7 +47,7 @@ middlewareObj.isLoggedIn = function(req, res, next){
     if(req.isAuthenticated()){
         return next();
     }
-    req.flash('error', "Please Login First!");
+    req.flash('error', "You need to be logged in to do that.");
     res.redirect("/login");
 }
 
