aa3d9720d33403647f70358f3f6b42fa5b327173
Wesley <wesleyrasada@gmail.com>
2017-02-25 21:35:21 -0800

    Added process.env.PORT

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -47,6 +47,6 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("Yelp Camp has started on port 3000");
 });
