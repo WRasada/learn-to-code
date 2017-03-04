9618d917da16007ba38b18df3d3afb262f8c8fb7
Wesley <wesleyrasada@gmail.com>
2017-03-03 23:12:51 -0800

    Node.JS object inheritance, prototype, function constructor

--- Node_JS/2nd_Course/app.js
+++ Node_JS/2nd_Course/app.js
@@ -1,23 +1,61 @@
-// function statement
-function greet(){
-  console.log('Hi!');
+// // function statement
+// function greet(){
+//   console.log('Hi!');
+// };
+// greet();
+//
+// // functions are first-class
+// function logGreeting(fn){
+//   fn();
+// };
+// logGreeting(greet);
+//
+// // function expression
+// var greetMe = function(){
+//   console.log('Hello!');
+// };
+// greetMe();
+//
+// logGreeting(greetMe);
+//
+// logGreeting(function(){
+//   console.log("hello world!");
+// });
+
+// var greet = require('./greet');
+
+// var person = {
+//   firstname: "John",
+//   lastname: 'Doe',
+//   greet: function() {
+//     console.log("Hello, " + this.firstname + ' ' + this.lastname);
+//   }
+// };
+//
+// person.greet();
+
+// function constructor
+function Person(firstname, lastname){
+  this.firstname = firstname;
+  this.lastname = lastname;
+}
+
+Person.prototype.id = 1;
+
+Person.prototype.greet = function(){
+  console.log("Hello, " + this.firstname + ' ' + this.lastname);
 };
-greet();
 
-// functions are first-class
-function logGreeting(fn){
-  fn();
-};
-logGreeting(greet);
+var john = new Person("John", "Doe");
 
-// function expression
-var greetMe = function(){
-  console.log('Hello!');
-};
-greetMe();
+var will = new Person('Will', 'Smith');
 
-logGreeting(greetMe);
+will.greet();
+john.greet();
+console.log(john.id);
+john.id = 2;
+console.log(john.id);
 
-logGreeting(function(){
-  console.log("hello world!");
-});
+console.log(john.__proto__);
+console.log(will.__proto__);
+console.log(john.__proto__ === will.__proto__);
