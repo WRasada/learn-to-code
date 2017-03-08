c075e5854e5e5b5837d9ada4a3151c2fa281d24a
Wesley <wesleyrasada@gmail.com>
2017-03-07 20:51:44 -0800

    Node.JS Outputting HTML templates

--- Node_JS/2nd_Course/server/app.js
+++ Node_JS/2nd_Course/server/app.js
@@ -1,8 +1,13 @@
 var http = require('http');
+var fs = require('fs');
+
 
 http.createServer(function(req, res){
 
-  res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.end('Hello World!\n');
+  res.writeHead(200, { 'Content-Type': 'text/html' })
+  var html = fs.readFileSync(__dirname + '/index.html', 'utf8');
+  var message = 'Hello world...';
+  html = html.replace('{Message}', message);
+  res.end(html);
 
 }).listen(3000);
