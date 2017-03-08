75f2ee4dfea8dfee4c03d9f00f786c4de2655c53
Wesley <wesleyrasada@gmail.com>
2017-03-07 21:12:16 -0800

    Node.JS JSON

--- Node_JS/2nd_Course/server/app2.js
+++ Node_JS/2nd_Course/server/app2.js
@@ -4,7 +4,11 @@ var fs = require('fs');
 
 http.createServer(function(req, res){
 
-  res.writeHead(200, { 'Content-Type': 'text/html' })
-  fs.createReadStream(__dirname + '/index.html').pipe(res);
+  res.writeHead(200, { 'Content-Type': 'application/json' })
+  var obj = {
+    firstname: 'John',
+    lastname: 'Doe'
+  };
+  res.end(JSON.stringify(obj));
 
 }).listen(3000);
