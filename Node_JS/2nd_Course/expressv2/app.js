fdf8b3cca5066bb93f26feaf0e06599ae6a66453
Wesley <wesleyrasada@gmail.com>
2017-03-08 20:07:11 -0800

    Node.JS QueryString Parameters and JSON

--- Node_JS/2nd_Course/expressv2/app.js
+++ Node_JS/2nd_Course/expressv2/app.js
@@ -1,10 +1,21 @@
-var express = require('express'),
-    app     = express();
+var express    = require('express'),
+    bodyParser = require('body-parser'),
+    app        = express();
 
 var port = process.env.PORT || 3000;
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+var jsonParser = bodyParser.json();
+
+app.use('/assets', express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+
+app.use('/', function(req, res, next){
+  console.log('Request URL: ' + req.url);
+  next();
+});
 
 app.get('/', function(req, res){
-  res.send("Hello");
+  res.render('index');
 });
 
 app.get('/api', function(req, res){
@@ -12,7 +23,18 @@ app.get('/api', function(req, res){
 });
 
 app.get('/person/:id', function(req, res){
-  res.send(`Hello ${req.params.id}!`)
+  res.render('person', { ID: req.params.id, Qstr: req.query.qstr })
 });
 
+app.post('/person', urlencodedParser, function(req, res){
+  res.send('Thank you!');
+  console.log(req.body.firstname);
+  console.log(req.body.lastname);
+});
+
+app.post('/personjson', jsonParser, function(req, res){
+  res.send('Thank you for the json data');
+  console.log(req.body.firstname);
+  console.log(req.body.lastname);
+})
 app.listen(port);
