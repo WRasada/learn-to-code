ab03d5e66cc1eba59e4680d625f1e65057b80057
Wesley <wesleyrasada@gmail.com>
2017-03-09 21:29:39 -0800

    Node.JS Angular Server/Client javascript

--- Angular/public/js/app.js
+++ Angular/public/js/app.js
@@ -3,17 +3,10 @@ angular.module('TestApp', []);
 angular.module('TestApp')
   .controller('MainController', ctrlFunc);
 
+
 function ctrlFunc() {
-  this.message = 'Hello';
-  this.people = [
-    {
-      name: 'John Doe'
-    },
-    {
-      name: 'Jane Doe'
-    },
-    {
-      name: 'Jim Doe'
-    }
-  ]
+
+
+  this.people = clientPeople;
+
 }
