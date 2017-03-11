16347b8882d79ba61fbdd1d8bf3e6990ae1d992b
Wesley <wesleyrasada@gmail.com>
2017-03-10 21:14:46 -0800

    User app create_test

--- MongoDB_Course/exercises/users/test/create_test.js
+++ MongoDB_Course/exercises/users/test/create_test.js
@@ -2,7 +2,14 @@ const assert = require('assert');
 const User = require('../src/user');
 
 describe('Creating records', () => {
-  it('saves a user', () => {
-    const joe = new User({ name: 'Joe' })
+  it('saves a user', (done) => {
+    const joe = new User({ name: 'Joe' });
+
+    joe.save()
+      .then(() => {
+        // Has joe been saved succesfully?
+        assert(!joe.isNew);
+        done();
+      });
   });
 });
