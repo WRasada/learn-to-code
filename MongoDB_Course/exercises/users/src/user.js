62e0f66527dfcc3fe116edeb68078cf77fd0d102
Wesley <wesleyrasada@gmail.com>
2017-03-11 10:31:41 -0800

    Mocha MongoDB validation final

--- MongoDB_Course/exercises/users/src/user.js
+++ MongoDB_Course/exercises/users/src/user.js
@@ -4,7 +4,14 @@ const Schema = mongoose.Schema;
 mongoose.Promise = global.Promise;
 
 const userSchema = new Schema({
-  name: String,
+  name: {
+    type: String,
+    validate: {
+      validator: (name) => name.length > 2,
+      message: 'Name must be longer than 2 characters.'
+    },
+    required: [true, 'Name is required.']
+  },
   postCount: Number
 });
 
