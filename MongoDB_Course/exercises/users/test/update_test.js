ae54774e9bd31f15f1dd428ba0dac1325132366a
Wesley <wesleyrasada@gmail.com>
2017-03-11 09:52:05 -0800

    Mocha MongoDB Update Operators

--- MongoDB_Course/exercises/users/test/update_test.js
+++ MongoDB_Course/exercises/users/test/update_test.js
@@ -5,7 +5,7 @@ describe('Updating records', () => {
   let joe;
 
   beforeEach((done) => {
-    joe = new User({name: 'Joe'})
+    joe = new User({name: 'Joe', postCount: 0})
     joe.save()
       .then(() => done());
   });
@@ -51,4 +51,12 @@ describe('Updating records', () => {
     );
   });
 
+  it('A user can have their post count incremented by 1', (done) => {
+    User.update({ name: 'Joe' }, { $inc: { postCount: 1 } })
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then((user) => {
+        assert(user.postCount === 1);
+        done();
+      });
+  });
 });
