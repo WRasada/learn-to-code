c6f2bd4e6903edaa586ac73d39628b526e210a80
Wesley <wesleyrasada@gmail.com>
2017-03-12 21:40:12 -0700

    Mocha MongoDB - Middleware_test final

--- MongoDB_Course/exercises/users/src/user.js
+++ MongoDB_Course/exercises/users/src/user.js
@@ -25,6 +25,12 @@ userSchema.virtual('postCount').get(function() {
   return this.posts.length;
 });
 
+userSchema.pre('remove', function(next) {
+  const BlogPost = mongoose.model('BlogPost');
+  BlogPost.remove({ _id: { $in: this.blogPosts } })
+    .then (() => next());
+});
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
