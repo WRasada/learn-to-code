3c46da9d8564c3aece604ce43eeccbe734f18f35
Wesley <wesleyrasada@gmail.com>
2017-03-12 21:17:11 -0700

    Mocha MongoDB - Associations_test final

--- MongoDB_Course/exercises/users/test/association_test.js
+++ MongoDB_Course/exercises/users/test/association_test.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const assert   = require('assert');
 const User     = require('../src/user');
 const Comment  = require('../src/comment');
 const BlogPost = require('../src/blogPost');
@@ -11,7 +12,43 @@ describe('Associations', () => {
     blogPost = new BlogPost({ title: 'JS is great!', content: 'This is great!' });
     comment = new Comment({ content: 'Congrats on post' });
 
-    joe.BlogPosts.push(blogPost);
+    joe.blogPosts.push(blogPost);
+    blogPost.comments.push(comment);
+    comment.user = joe;
+
+    Promise.all([joe.save(), blogPost.save(), comment.save()])
+      .then(() => done());
+  });
+
+  it('saves a relation between a user and a blogpost', (done) => {
+    User.findOne({ name: 'Joe' })
+      .populate('blogPosts')
+      .then((user) => {
+        assert(user.blogPosts[0].title === 'JS is great!')
+        done();
+      });
+  });
+
+  it('saves a full relation graph', (done) => {
+    User.findOne({ name: 'Joe' })
+      .populate({
+        path: 'blogPosts',
+        populate: {
+          path: 'comments',
+          model: 'Comment',
+          populate: {
+            path: 'user',
+            model: 'User'
+          }
+        }
+      })
+      .then((user) => {
+        assert(user.name === 'Joe');
+        assert(user.blogPosts[0].title === 'JS is great!');
+        assert(user.blogPosts[0].comments[0].content === 'Congrats on post');
+        assert(user.blogPosts[0].comments[0].user.name === 'Joe');
+        done();
+      });
   });
 
 });
