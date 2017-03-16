5158f1c7b53dc03ce19a8611de0c31ec35a8bea4
Wesley <wesleyrasada@gmail.com>
2017-03-15 21:58:01 -0700

    Added driver_edit test, installed bluebird

--- MongoDB_Course/exercises/muber/app.js
+++ MongoDB_Course/exercises/muber/app.js
@@ -4,8 +4,6 @@ const mongoose = require('mongoose');
 const routes = require('./routes/routes');
 const app = express();
 
-mongoose.Promise = global.Promise;
-
 if (process.env.NODE_ENV !== 'test') {
   mongoose.connect('mongodb://localhost/muber');
 }
