f32bdacbc43a9c11ebec10f4af94eeab97fac007
Wesley <wesleyrasada@gmail.com>
2017-03-15 23:19:50 -0700

    Driver model GeoJSON subdocument

--- MongoDB_Course/exercises/muber/models/driver.js
+++ MongoDB_Course/exercises/muber/models/driver.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 mongoose.Promise = require('bluebird');
 
+const PointSchema = new Schema({
+  type: { type: String, default: 'Point' },
+  coordinates: { type: [Number], index: '2dsphere' }
+})
+
 const DriverSchema = new Schema({
   email: {
     type: String,
@@ -10,7 +15,9 @@ const DriverSchema = new Schema({
   driving: {
     type: Boolean,
     default: false
-  }
+  },
+  geometry: PointSchemca
+
 });
 
 const Driver = mongoose.model('driver', DriverSchema);
