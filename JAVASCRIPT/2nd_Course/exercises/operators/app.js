2b315347384c94d39fd7ed8006a3b552625fd0bd
Wesley <wesleyrasada@gmail.com>
2017-03-19 21:11:11 -0700

    Operators

---
+++ JAVASCRIPT/2nd_Course/exercises/operators/app.js
@@ -0,0 +1,37 @@
+// var a = 2, b = 3, c = 4;
+//
+// a = b = c;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+//
+// var a = (3 + 4) * 7;
+//
+// console.log(a);
+//
+
+// var b = 3 === 3;
+// console.log(b);
+//
+// var a = 0;
+// var b = false;
+//
+// if (a === b) {
+//   console.log('They are equal');
+// } else {
+//   console.log('They are not equal');
+// }
+//
+// var a;
+//
+// if (a) {
+//   console.log('something is there');
+// }
+
+function greet(name) {
+  name = name || '<Your name here>';
+  console.log('Hello c' + name);
+}
+
+greet('Tony');
+greet();
