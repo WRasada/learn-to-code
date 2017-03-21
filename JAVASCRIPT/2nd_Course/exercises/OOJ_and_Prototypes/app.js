08ba29d1afcade5308777d816c2db03dac076201
Wesley <wesleyrasada@gmail.com>
2017-03-20 23:05:21 -0700

    Understanding the Prototype

---
+++ JAVASCRIPT/2nd_Course/exercises/OOJ_and_Prototypes/app.js
@@ -0,0 +1,24 @@
+var person = {
+  firstname: 'Default',
+  lastname: 'Default',
+  getFullName: function() {
+    return this.firstname + ' ' + this.lastname;
+  }
+}
+
+var john = {
+  firstname: 'John',
+  lastname: 'Doe'
+}
+
+john.__proto__ = person;
+
+console.log(john.getFullName());
+
+var jane = {
+  firstname: 'Jane'
+}
+
+jane.__proto__ = person;
+
+console.log(jane.getFullName());
