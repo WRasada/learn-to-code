e5312d8151d6a11fc58c788eb30fc1d1acdfc54b
Wesley <wesleyrasada@gmail.com>
2017-03-21 18:23:40 -0700

    Function Constructors and .prototype

--- JAVASCRIPT/2nd_Course/exercises/obj_and_functions/app.js
+++ JAVASCRIPT/2nd_Course/exercises/obj_and_functions/app.js
@@ -205,42 +205,64 @@
 
 // .call(), .apply() and .bind()
 
-var person = {
-  firstname: 'John',
-  lastname: 'Doe',
-  getFullName: function() {
-    var fullname = this.firstname + ' ' + this.lastname;
-    return fullname;
-  }
+// var person = {
+//   firstname: 'John',
+//   lastname: 'Doe',
+//   getFullName: function() {
+//     var fullname = this.firstname + ' ' + this.lastname;
+//     return fullname;
+//   }
+// }
+//
+// var logName = function(lang1, lang2) {
+//   console.log('Logged: ' + this.getFullName());
+//   console.log('Arguments: ' + lang1 + ' ' + lang2);
+//   console.log('-----------');
+// }
+//
+// var logPersonName = logName.bind(person);
+// logPersonName('en');
+//
+// logName.call(person, 'en', 'es');
+// logName.apply(person, ['en', 'es']);
+//
+//
+// // function borrowing
+//
+// var person2 = {
+//   firstname: 'Jane',
+//   lastname: 'Doe'
+// }
+//
+// console.log(person.getFullName.apply(person2));
+//
+// // function currying
+//
+// function multiply(a, b) {
+//   return a * b;
+// }
+//
+// var multiplyByTwo = multiply.bind(this, 2);
+// console.log(multiplyByTwo(4));
+
+// Function Constructors & 'New' & .prototype
+
+function Person(firstname, lastname) {
+  console.log(this);
+  this.firstname = firstname;
+  this.lastname = lastname;
 }
 
-var logName = function(lang1, lang2) {
-  console.log('Logged: ' + this.getFullName());
-  console.log('Arguments: ' + lang1 + ' ' + lang2);
-  console.log('-----------');
+Person.prototype.getFullName = function() {
+  return this.firstname + ' ' + this.lastname;
 }
 
-var logPersonName = logName.bind(person);
-logPersonName('en');
+var john = new Person('John', 'Doe');
 
-logName.call(person, 'en', 'es');
-logName.apply(person, ['en', 'es']);
+console.log(john);
 
-
-// function borrowing
-
-var person2 = {
-  firstname: 'Jane',
-  lastname: 'Doe'
+Person.prototype.getFormalFullName = function() {
+  return this.lastname + ', ' + this.firstname;
 }
 
-console.log(person.getFullName.apply(person2));
-
-// function currying
-
-function multiply(a, b) {
-  return a * b;
-}
-
-var multiplyByTwo = multiply.bind(this, 2);
-console.log(multiplyByTwo(4));
+console.log(john.getFormalFullName());
