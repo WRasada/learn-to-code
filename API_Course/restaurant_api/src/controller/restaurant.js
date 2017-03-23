9ab91319558565cad221ac2a3f396da2b0009f1a
Wesley <wesleyrasada@gmail.com>
2017-03-22 22:23:59 -0700

    Add GET method

--- API_Course/restaurant_api/src/controller/restaurant.js
+++ API_Course/restaurant_api/src/controller/restaurant.js
@@ -5,7 +5,7 @@ import Restaurant from '../model/restaurant';
 export default({ config, db }) => {
   let api = Router();
 
-  // '/v1/restaurant/add'
+  // '/v1/restaurant/add' - Create
   api.post('/add', (req, res) => {
     let newRest = new Restaurant();
     newRest.name = req.body.name;
@@ -18,5 +18,25 @@ export default({ config, db }) => {
     });
   });
 
+  // '/v1/restaurant' - Read
+  api.get('/', (req, res) => {
+    Restaurant.find({}, (err, restaurants) => {
+      if (err) {
+        res.send(err);
+      }
+      res.json(restaurants);
+    });
+  });
+
+  // '/v1/restaurant/:id' - Read 1
+  api.get('/:id', (req, res) => {
+    Restaurant.findById(req.params.id, (err, restaurant) => {
+      if (err) {
+        res.send(err);
+      }
+      res.json(restaurant);
+    });
+  });
+  
   return api;
 }
