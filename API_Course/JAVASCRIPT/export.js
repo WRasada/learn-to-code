002fbc38ec87a214fd85e425f5a53f708d34f67f
Wesley <wesleyrasada@gmail.com>
2017-03-22 20:58:32 -0700

    Javascript ES6 import

---
+++ API_Course/JAVASCRIPT/export.js
@@ -0,0 +1,17 @@
+module.exports = {
+
+  myVar: 942,
+
+  func1: () => {
+    console.log('Hello from func1');
+  },
+
+  func2: () => {
+    console.log('Hello from func2');
+  }
+
+  // module.exports.myVar = myVariable;
+  // module.exports.func1 = func1;
+  // module.exports.func2 = func2;
+
+}
