b880325a45f43d306cc270f8440f5eb56091ad82
Wesley <wesleyrasada@gmail.com>
2017-03-23 21:38:26 -0700

    Refactor code and files

--- Node_JS/3rd_Course/notes_node/app.js
+++ Node_JS/3rd_Course/notes_node/app.js
@@ -1,20 +1,6 @@
 console.log('Starting app.js');
 
 const fs = require('fs');
-const os = require('os');
 const _  = require('lodash');
+
 const notes = require('./notes');
-
-// console.log(_.isString(true));
-// console.log(_.isString('Wes'));
-let filteredArray = _.uniq([1, 2, 3, 4, 1, 'Wes', 'Wes', 'Andrew']);
-console.log(filteredArray);
-// var res = notes.addNote();
-// console.log(res);
-//
-// var res2 = notes.add(3, 4);
-// console.log(res2);
-
-// let user = os.userInfo();
-//
-// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`);
