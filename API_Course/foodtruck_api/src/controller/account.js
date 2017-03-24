b487b53aa72674aa2c91d3f75702df50a1bd301b
Wesley <wesleyrasada@gmail.com>
2017-03-23 17:36:13 -0700

    Add User Auth

---
+++ API_Course/foodtruck_api/src/controller/account.js
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose';
+import { Router } from 'express';
+import Account from '../model/account';
+import bodyParser from 'body-parser';
+import passport from 'passport':
+import config from '../config';
+
+import { generateAccessToken, respond, authenticate } from '../middleware/authMiddleware';
+
+export default ({ config, db }) => {
+  let api = Router();
+
+  // '/v1/account'
+  api.post('/register', (req, res) => {
+    Account.register(new Account({ username: req.body.email }), req.body.password, function(err, account) {
+      if (err) {
+        res.send(err);
+      }
+
+      passport.authenticate(
+        'local', {
+          session: false
+        })(req, res, () => {
+          res.status(200).send('Successfully created new account')
+        });
+      });
+  });
+
+  // '/v1/account/login'
+  api.post('/login', passport.authenticate(
+    'local', {
+      session: false,
+      scope: []
+    }), generateAccessToken, respond
+  ));
+
+  return api;
+}
