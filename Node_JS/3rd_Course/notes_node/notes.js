fc75034d03cdc20e4338212e4551cde075dd851a
Wesley <wesleyrasada@gmail.com>
2017-03-23 22:50:14 -0700

    Add and Save Notes

--- Node_JS/3rd_Course/notes_node/notes.js
+++ Node_JS/3rd_Course/notes_node/notes.js
@@ -1,7 +1,27 @@
 console.log('Starting notes.js');
 
+const fs = require('fs');
+
 let addNote = (title, body) => {
-  console.log('Adding note:', title, body);
+  let notes = [];
+  let note = {
+    title,
+    body
+  };
+
+  try {
+    let notesString = fs.readFileSync('notes_data.json');
+    notes = JSON.parse(notesString);
+  } catch (e) {
+
+  }
+
+  let duplicateNotes = notes.filter((note) => note.title === title);
+
+  if (duplicateNotes.length === 0) {
+    notes.push(note);
+    fs.writeFileSync('notes_data.json', JSON.stringify(notes));
+  }
 };
 
 let getAll = () => {
