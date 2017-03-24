7e8c9e40470142d8cec336c76874ceb71ab524b5
Wesley <wesleyrasada@gmail.com>
2017-03-23 21:29:59 -0700

    Requiring 3rd party modules

--- Node_JS/3rd_Course/notes_node/app.js
+++ Node_JS/3rd_Course/notes_node/app.js
@@ -2,13 +2,18 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _  = require('lodash');
 const notes = require('./notes');
 
-var res = notes.addNote();
-console.log(res);
-
-var res2 = notes.add(3, 4);
-console.log(res2);
+// console.log(_.isString(true));
+// console.log(_.isString('Wes'));
+let filteredArray = _.uniq([1, 2, 3, 4, 1, 'Wes', 'Wes', 'Andrew']);
+console.log(filteredArray);
+// var res = notes.addNote();
+// console.log(res);
+//
+// var res2 = notes.add(3, 4);
+// console.log(res2);
 
 // let user = os.userInfo();
 //
