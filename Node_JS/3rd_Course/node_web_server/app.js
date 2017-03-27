18a496510220f58621d81b9cac73fd19469865ee
Wesley <wesleyrasada@gmail.com>
2017-03-26 18:50:23 -0700

    Rendering templates with data

--- Node_JS/3rd_Course/node_web_server/app.js
+++ Node_JS/3rd_Course/node_web_server/app.js
@@ -1,19 +1,24 @@
 const express = require('express');
+const hbs     = require('hbs');
+
 const app     = express();
 
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'))
+
 app.get('/', (req, res) => {
-  // res.send('<h1>Hello Express!</h1>');
-  res.send({
-    name: 'Wes',
-    likes: [
-      'japanese',
-      'Anime'
-    ]
+  res.render('home', {
+    pageTitle: 'Home Page',
+    currentYear: new Date().getFullYear(),
+    welcomeMessage: 'Welcome to my Site!'
   });
 });
 
 app.get('/about', (req, res) => {
-  res.send('About Page');
+  res.render('about', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  });
 });
 
 app.get('/bad', (req, res) => {
@@ -23,4 +28,8 @@ app.get('/bad', (req, res) => {
 });
 
 
-app.listen(3000);
+
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
