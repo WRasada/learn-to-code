14471f0c326ee46afc95e5e6c7d9a9d7336b2735
Wesley <wesleyrasada@gmail.com>
2017-03-27 21:23:57 -0700

    MongoDB ObjectID practice

--- Node_JS/3rd_Course/node_to_api/playground/mongodb_connect.js
+++ Node_JS/3rd_Course/node_to_api/playground/mongodb_connect.js
@@ -1,4 +1,5 @@
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
 
 MongoClient.connect('mongodb://localhost/TodoApp', (err, db) => {
   if (err) {
@@ -15,16 +16,18 @@ MongoClient.connect('mongodb://localhost/TodoApp', (err, db) => {
   //   }
   //   console.log(JSON.stringify(result.ops, undefined, 2));
   // });
-  db.collection('Users').insertOne({
-    name: 'Wesley',
-    age: 27,
-    location: 'Post Falls, ID'
-  }, (err, result) => {
-    if (err) {
-      return console.log('Unable to insert user', err)
-    }
-    console.log(JSON.stringify(result.ops, undefined, 2));
-  });
+  // db.collection('Users').insertOne({
+  //   name: 'Wesley',
+  //   age: 27,
+  //   location: 'Post Falls, ID'
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert user', err)
+  //   }
+  //   console.log(JSON.stringify(result.ops[0]._id.getTimestamp(), undefined, 2));
+  // });
+
+
 
   db.close();
 });
