04d7f02b4056f2d6366d95aa321d99b564673198
Wesley <wesleyrasada@gmail.com>
2017-03-28 19:50:20 -0700

    Add GET /todos route

--- Node_JS/3rd_Course/node_to_api/server/server.js
+++ Node_JS/3rd_Course/node_to_api/server/server.js
@@ -20,7 +20,15 @@ app.post('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   });
-})
+});
+
+app.get('/todos', (req, res) => {
+  let todos = Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
 
 app.listen(port, () => {
   console.log(`Started on port: ${port}`);
