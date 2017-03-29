fa93e960b17ff7522dece1ab47335e5b86ecf394
Wesley <wesleyrasada@gmail.com>
2017-03-28 22:49:13 -0700

    Test the DELETE /todos/:id route

--- Node_JS/3rd_Course/node_to_api/server/tests/server.test.js
+++ Node_JS/3rd_Course/node_to_api/server/tests/server.test.js
@@ -80,26 +80,63 @@ describe('GET /todos/:id', () => {
     let id = todos[0]._id.toHexString();
 
     request(app)
-    .get(`/todos/${id}`)
-    .expect(200)
-    .expect((res) => {
-      expect(res.body.todo.text).toBe(todos[0].text)
-    })
-    .end(done);
+      .get(`/todos/${id}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(todos[0].text)
+      })
+      .end(done);
   });
 
   it('should return 404 for non-object ids', (done) => {
     request(app)
-    .get('/todos/123')
-    .expect(404)
-    .end(done);
+      .get('/todos/123')
+      .expect(404)
+      .end(done);
   });
 
   it('should return 404 if todo not found', (done) => {
     let fakeId = new ObjectID().toHexString();
     request(app)
-    .get(`/todos/${fakeId}`)
-    .expect(404)
-    .end(done);
+      .get(`/todos/${fakeId}`)
+      .expect(404)
+      .end(done);
+  });
+});
+
+describe('DELETE /todos/:id', () => {
+  it('should remove a todo', (done) => {
+    let id = todos[0]._id.toHexString();
+
+    request(app)
+      .delete(`/todos/${id}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo._id).toBe(id);
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        Todo.findById(id).then((todo) => {
+          expect(todo).toNotExist();
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+
+  it('should return 404 if object id is invalid', (done) => {
+    request(app)
+      .delete('/todos/123')
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 404 if todo not found', (done) => {
+    let fakeId = new ObjectID().toHexString();
+    request(app)
+      .delete(`/todos/${fakeId}`)
+      .expect(404)
+      .end(done);
   });
 });
