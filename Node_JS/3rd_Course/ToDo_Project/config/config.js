f2ee2c446f3551bda8f1b7b718e8c64b7d9beed3
Wesley <wesleyrasada@gmail.com>
2017-04-13 18:11:38 -0700

    Update config to use json file

--- Node_JS/3rd_Course/ToDo_Project/config/config.js
+++ Node_JS/3rd_Course/ToDo_Project/config/config.js
@@ -1,9 +1,18 @@
 let env = process.env.NODE_ENV || 'development';
 
-if (env === 'development') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost/ToDoAppV1';
-} else if (env === 'test') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost/ToDoAppV1Tests';
+if(env === 'development' || env === 'test') {
+  let config = require('./config.json');
+  let envConfig = config[env];
+
+  Object.keys(envConfig).forEach((key) => {
+    process.env[key] = envConfig[key];
+  });
 }
+
+// if (env === 'development') {
+//   process.env.PORT = 3000;
+//   process.env.MONGODB_URI = 'mongodb://localhost/ToDoAppV1';
+// } else if (env === 'test') {
+//   process.env.PORT = 3000;
+//   process.env.MONGODB_URI = 'mongodb://localhost/ToDoAppV1Tests';
+// }
