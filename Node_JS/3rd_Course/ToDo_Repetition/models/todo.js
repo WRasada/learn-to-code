b0f9dc68ab58c2d30ffe0b13df3eef5c7dc87edf
Wesley <wesleyrasada@gmail.com>
2017-04-13 22:15:26 -0700

    Setup Todo Model

---
+++ Node_JS/3rd_Course/ToDo_Repetition/models/todo.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let TodoSchema = new Schema({
+  text: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  },
+  completedAt: {
+    type: Number,
+    default: null
+  }
+});
+
+const Todo = mongoose.model('Todo', TodoSchema);
+
+module.exports = { Todo };
