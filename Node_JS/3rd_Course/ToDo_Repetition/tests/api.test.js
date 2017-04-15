d48d1dd6f0b591bca83fc5644d34b0a6b644fc40
Wesley <wesleyrasada@gmail.com>
2017-04-14 19:38:41 -0700

    Add POST /todos tests

--- Node_JS/3rd_Course/ToDo_Repetition/tests/api.test.js
+++ Node_JS/3rd_Course/ToDo_Repetition/tests/api.test.js
@@ -19,3 +19,45 @@ describe('GET /todos', () => {
       .end(done);
   })
 });
+
+describe('POST /todos', () => {
+  it('should create new todo', (done) => {
+    let text = 'This is my new todo';
+
+    request(app)
+      .post('/todos')
+      .send({ text })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(text);
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        Todo.find({ text }).then((todo) => {
+          expect(todo.length).toBe(1);
+          expect(todo[0].text).toBe(text);
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+
+  it('should not create todo with invalid data', done => {
+    request(app)
+      .post('/todos')
+      .send({})
+      .expect(400)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        Todo.find().then((todos) => {
+          expect(todos.length).toBe(2);
+          done();
+        }).catch((e) => done(e));
+      })
+  });
+});
