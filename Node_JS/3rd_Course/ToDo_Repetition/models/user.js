c269b7aec9268b2e207cf90d7924c7a6fa7a5df5
Wesley <wesleyrasada@gmail.com>
2017-04-15 12:57:14 -0700

    Add User Model

--- Node_JS/3rd_Course/ToDo_Repetition/models/user.js
+++ Node_JS/3rd_Course/ToDo_Repetition/models/user.js
@@ -5,3 +5,30 @@ const bcrypt      = require('bcrypstjs');
 const _           = require('lodash');
 
 const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    trim: true,
+    minlength: 1,
+    required: true,
+    unique: true,
+    validate: {
+      validator: validaotr.isEmail,
+      message: '{VALUE} is not a valid email'
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  tokens: [{
+    access: {
+      type: String,
+      required: true
+    }
+  }]
+})
+
+const User = mongoose.model('User', UserSchema);
