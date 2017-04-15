e7ef07d4d454bd269394819cd987971870ef8e72
Wesley <wesleyrasada@gmail.com>
2017-04-14 22:01:53 -0700

    Add PATCH /todos/:id tests

--- Node_JS/3rd_Course/ToDo_Repetition/tests/api.test.js
+++ Node_JS/3rd_Course/ToDo_Repetition/tests/api.test.js
@@ -91,3 +91,43 @@ describe('GET /todos/:id', () => {
       .end(done);
   });
 });
+
+describe('PATCH /todos/:id', () => {
+  it('should update a todo', (done) => {
+    let id = todos[0]._id.toHexString();
+    let text = 'New and improved';
+
+    request(app)
+      .patch(`/todos/${id}`)
+      .send({
+        completed: true,
+        text
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(text);
+        expect(res.body.todo.completed).toBe(true);
+        expect(res.body.todo.completedAt).toBeA('string');
+      })
+      .end(done);
+  });
+
+  it('should clear completedAt when todo is not completed', (done) => {
+    let id = todos[1]._id.toHexString();
+    let text = 'New and Improved v2';
+
+    request(app)
+      .patch(`/todos/${id}`)
+      .send({
+        completed: false,
+        text
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(text);
+        expect(res.body.todo.completed).toBe(false);
+        expect(res.body.todo.completedAt).toNotExist;
+      })
+      .end(done);
+  });
+});
