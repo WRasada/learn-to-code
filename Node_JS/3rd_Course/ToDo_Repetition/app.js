1c7b80c6e845f64502ade36e663ab78a0dffc3b5
Wesley <wesleyrasada@gmail.com>
2017-04-14 20:55:25 -0700

    Add ObjectID check to GET /todos/:id route

--- Node_JS/3rd_Course/ToDo_Repetition/app.js
+++ Node_JS/3rd_Course/ToDo_Repetition/app.js
@@ -48,6 +48,10 @@ app.post('/todos', (req, res) => {
 app.get('/todos/:id', (req, res) => {
   let id = req.params.id;
 
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
   Todo.findById(id).then((todo) => {
     if (!todo) {
       return res.status(404).send();
